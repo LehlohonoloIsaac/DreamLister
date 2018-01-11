a8447fa201c5c29f5f817a78197dc8f7412d32bd
Lehlohonolo <lehlohonoloisaac25@gmail.com>
2018-01-11 13:42:23 +0200

    Update item creation date whenever an item is added

--- DreamLister/Model/Item+CoreDataClass.swift
+++ DreamLister/Model/Item+CoreDataClass.swift
@@ -13,4 +13,8 @@ import CoreData
 @objc(Item)
 public class Item: NSManagedObject {
 
+    public override func awakeFromInsert() {
+        super.awakeFromInsert()
+        self.created = NSDate()
+    }
 }
