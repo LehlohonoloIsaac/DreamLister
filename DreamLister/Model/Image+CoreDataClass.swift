7402638a7e39ae4d879b654eb803d057a56d3857
Lehlohonolo <lehlohonoloisaac25@gmail.com>
2018-01-11 13:33:49 +0200

    Create Data Models

---
+++ DreamLister/Model/Image+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Image+CoreDataClass.swift
+//  DreamLister
+//
+//  Created by dvt on 2018/01/11.
+//  Copyright © 2018 dvt. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Image)
+public class Image: NSManagedObject {
+
+}
