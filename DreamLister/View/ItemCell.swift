4ce84a4d088bd3bcb48dc1af0b5ae5d43520b5b4
Lehlohonolo <lehlohonoloisaac25@gmail.com>
2018-01-11 14:48:46 +0200

    Connect the views which their associated Controllers

---
+++ DreamLister/View/ItemCell.swift
@@ -0,0 +1,18 @@
+//
+//  ItemCell.swift
+//  DreamLister
+//
+//  Created by dvt on 2018/01/11.
+//  Copyright © 2018 dvt. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+
+    @IBOutlet weak var thumb: UIImageView!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var price: UILabel!
+    @IBOutlet weak var details: UILabel!
+    
+}
